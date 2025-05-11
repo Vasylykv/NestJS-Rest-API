1341da9948cba064da59ffec8c788a76d3ba8666
Vasylykv <fygetoid@gmail.com>
2025-05-11 18:15:39 +0300

    implement user roles

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from 'src/users/users.model';
+import { UserRoles } from './user-roles.model';
+
+interface RoleCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ tableName: 'roles', timestamps: false })
+export class Role extends Model<Role, RoleCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Unique ID' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  declare id: number;
+
+  @ApiProperty({ example: 'ADMIN', description: "User's role" })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  value: string;
+
+  @ApiProperty({ example: 'Admininstrator', description: "Role's description" })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
+}
